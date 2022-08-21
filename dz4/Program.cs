47764bbfffb7fa64c8db91899098d3ab5ec3c3c3
Kashira2 <nekit.egorov@icloud.com>
2022-08-21 15:42:25 +0300

    last commit

---
+++ dz4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= N; i = i+2)
+{
+    Console.WriteLine(i + ", ");
+}
+*/
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while ( i <= N)
+{
+    if ( i % 2 == 0)
+    {
+        Console.WriteLine(i + ", ");
+    }
+    i++;
+}
